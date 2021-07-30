048880b6643d32214fb960fe1686d5335af7a409
Sam Frances <sam@samfrances.uk>
2021-07-30 09:27:06 +0100

    Parser complete apart from top-level formulas without surrounding parentheses

--- src/sequent_parser.yrl
+++ src/sequent_parser.yrl
@@ -1,12 +1,22 @@
-Nonterminals sequent formula formulas.
-Terminals atom '|-' ','.
+Nonterminals sequent formula formulas binary_op unary_op.
+Terminals atom '|-' ',' '(' ')' binary unary.
 Rootsymbol sequent.
 
 sequent    ->  '|-' formula           : ['$2'].
 sequent    ->  formulas '|-' formula  : append('$1', ['$3']).
+
 formulas   ->  formula                : ['$1'].
 formulas   ->  formula ',' formulas   : ['$1'|'$3'].
-formula    ->  atom                   : extract_token('$1').
+
+formula    ->  atom                             : extract_token('$1').
+formula    -> '(' formula binary_op formula ')' : {'$3', '$2', '$4'}.
+formula    -> unary_op formula                  : {'$1', '$2'}.
+% overbracketing
+formula    -> '(' formula ')'                   : '$2'.
+
+binary_op  ->  binary   : extract_token('$1').
+unary_op   ->  unary    : extract_token('$1').
+
 
 
 Erlang code.
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -28,4 +28,46 @@ defmodule ParserTest do
     end
   end
 
+  test "should parse binary formulas with atomic children" do
+    assert AnalyticTableaux.Parser.parse("|- (p & q)") == [{:and, :p, :q}]
+    assert AnalyticTableaux.Parser.parse("|- (p | q)") == [{:or, :p, :q}]
+    assert AnalyticTableaux.Parser.parse("(a&b), (b&c),b|- (p & q)") == [{:and, :a, :b}, {:and, :b, :c}, :b, {:and, :p, :q}]
+    assert AnalyticTableaux.Parser.parse("(a&b), (b|c),b|- (p -> q)") == [{:and, :a, :b}, {:or, :b, :c}, :b, {:implies, :p, :q}]
+  end
+
+  test "should parse binary formulas with complex binary children" do
+    assert AnalyticTableaux.Parser.parse("|- (p & (q | r))") == [{:and, :p, {:or, :q, :r}}]
+    assert AnalyticTableaux.Parser.parse("p, ((q|r)|(a|(b&c)))|- (p & (q | r))") == [
+      :p,
+      {:or, {:or, :q, :r}, {:or, :a, {:and, :b, :c}}},
+      {:and, :p, {:or, :q, :r}}
+    ]
+  end
+
+  test "should parse unary formulas with atomic children" do
+    assert AnalyticTableaux.Parser.parse("|- ~q") == [{:not, :q}]
+    assert AnalyticTableaux.Parser.parse("|- ~a") == [{:not, :a}]
+    assert AnalyticTableaux.Parser.parse("~b |- a") == [{:not, :b}, :a]
+    assert AnalyticTableaux.Parser.parse("~b,~c |- a") == [{:not, :b}, {:not, :c}, :a]
+  end
+
+  test "should parse unary formulas with complex children" do
+    assert AnalyticTableaux.Parser.parse("|- ~~q") == [{:not, {:not, :q}}]
+    assert AnalyticTableaux.Parser.parse("|- ~(q&r)") == [{:not, {:and, :q, :r}}]
+    assert AnalyticTableaux.Parser.parse("|- ~~(q&r)") == [{:not, {:not, {:and, :q, :r}}}]
+    assert AnalyticTableaux.Parser.parse("p, ~d, (~(q|~r)|(a|~(b&c)))|- (p & (q | r))") == [
+      :p,
+      {:not, :d},
+      {:or, {:not, {:or, :q, {:not, :r}}}, {:or, :a, {:not, {:and, :b, :c}}}},
+      {:and, :p, {:or, :q, :r}}
+    ]
+  end
+
+  test "overbracketing is allowed" do
+    assert AnalyticTableaux.Parser.parse("|- (q)") == [:q]
+    assert AnalyticTableaux.Parser.parse("|- (~(~(q)))") == [{:not, {:not, :q}}]
+    assert AnalyticTableaux.Parser.parse("((a)) |- (q)") == [:a, :q]
+    assert AnalyticTableaux.Parser.parse("((a & b)) |- (q)") == [{:and, :a, :b}, :q]
+  end
+
 end
