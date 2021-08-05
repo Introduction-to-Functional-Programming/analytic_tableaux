104b3d9d821f9a2dc43842b9576f15d9783863c1
Adolfo Neto <adolfo.usp@gmail.com>
2021-08-05 19:12:03 -0300

    Tests for PHP generator

--- test/problems/problem_generator_test.exs
+++ test/problems/problem_generator_test.exs
@@ -2,11 +2,17 @@ defmodule ProblemGeneratorTest do
   use ExUnit.Case
 
   test "PHP1 is (p1_1&p2_1) |- (p1_1 & p2_1)" do
-    assert ProblemGenerator.generate(:php, 1) == "(p1_1 & p2_1) |- (p1_1 & p2_1)"
+    assert ProblemGenerator.generate(:php, 1) ==
+             "(p1_1&p2_1) |- (p1_1&p2_1)"
   end
 
   test "PHP2 is ..." do
     assert ProblemGenerator.generate(:php, 2) ==
-             "((p1_1|p1_2)&(p2_1|p2_2)&(p3_1|p3_2)) |- (p1,1&p2,1)|(p1,1&p3,1)|(p2,1&p3,1)|(p1,2&p2,2)|(p1,2&p3,2)|(p2,2&p3,2)"
+             "((p1_1|p1_2)&(p2_1|p2_2)&(p3_1|p3_2)) |- ((p1_1&p2_1)|(p1_1&p3_1)|(p2_1&p3_1)|(p1_2&p2_2)|(p1_2&p3_2)|(p2_2&p3_2))"
+  end
+
+  test "PHP3 is ..." do
+    assert ProblemGenerator.generate(:php, 3) ==
+             "((p1_1|p1_2|p1_3)&(p2_1|p2_2|p2_3)&(p3_1|p3_2|p3_3)&(p4_1|p4_2|p4_3)) |- ((p1_1&p2_1)|(p1_1&p3_1)|(p2_1&p3_1)|(p2_1&p4_1)|(p3_1&p4_1)|(p1_2&p2_2)|(p1_2&p3_2)|(p2_2&p3_2)|(p2_2&p4_2)|(p3_2&p4_2)|(p1_3&p2_3)|(p1_3&p3_3)|(p2_3&p3_3)|(p2_3&p4_3)|(p3_3&p4_3))"
   end
 end
