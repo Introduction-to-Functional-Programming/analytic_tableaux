22ec640c674aa640b2ea04a6419b31cb5810d3ad
Sam Frances <sam@samfrances.uk>
2021-08-07 09:33:11 +0100

    Get top-level roblem tests from problem files

--- test/analytic_tableaux_test.exs
+++ test/analytic_tableaux_test.exs
@@ -1,25 +1,22 @@
 defmodule AnalyticTableauxTest do
   use ExUnit.Case
 
-  @tag :pending
-  test "The sequent p, p->q |- q is valid" do
-    sequent = "p, p->q |- q"
+  TestHelpers.SequentExamples.valid()
+  |> Enum.each(fn sequent ->
+    @tag sequent: sequent
+    @tag :pending
+    test "The sequent #{sequent} is valid", context do
+      assert Prover.prove(context.sequent).status == :valid
+    end
+  end)
 
-    # signed_formulas = generate_signed_formulas(sequent)
-    # """
-    # T p
-    # T p->q
-    # F q
-    # """
+  TestHelpers.SequentExamples.invalid()
+  |> Enum.each(fn sequent ->
+    @tag sequent: sequent
+    @tag :pending
+    test "The sequent #{sequent} is NOT valid", context do
+      assert Prover.prove(context.sequent).status == :not_valid
+    end
+  end)
 
-    # Parser.parse(signed_formulas)
-
-    assert Prover.prove(sequent).status == :valid
-  end
-
-  @tag :pending
-  test "The sequent p, p->r |- q is NOT valid" do
-    sequent = "p, p->r |- q"
-    assert Prover.prove(sequent).status == :not_valid
-  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,24 @@
+defmodule TestHelpers.SequentExamples do
+
+  def valid() do
+    "./valid.problems"
+    |> Path.expand(__DIR__)
+    |> read_problem_file()
+  end
+
+  def invalid() do
+    "./invalid.problems"
+    |> Path.expand(__DIR__)
+    |> read_problem_file()
+  end
+
+  defp read_problem_file(path) do
+    path
+    |> File.read!
+    |> String.split("\n")
+  end
+
+end
+
 ExUnit.start()
 ExUnit.configure(exclude: :pending, trace: true)
